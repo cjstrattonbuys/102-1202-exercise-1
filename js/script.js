4a3d5dc4c27edfe4622137e3c6b3346d07a27565
CJ Buys <cjstrattonbuys@gmail.com>
2022-02-22 16:00:47 -0500

    Allow variation to the number of users

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 const randomFolks = document.querySelector(".random-peeps");
+const selectUserNumber = document.querySelector(".num-users");
 
-const getData = async function () {
-    const usersRequest = await fetch ("https://randomuser.me/api?results=5");
+const getData = async function (numUsers) {
+    const usersRequest = await fetch (`https://randomuser.me/api?results=${numUsers}`);
     const data = await usersRequest.json();   
     //console.log(data);
     const userResults = data.results;
@@ -9,7 +10,7 @@ const getData = async function () {
     displayUsers(userResults);
 }
 
-getData();
+getData(1);
 
 const displayUsers = function (userResults) {
     randomFolks.innerHTML = "";
@@ -28,3 +29,9 @@ const displayUsers = function (userResults) {
     }
 };
 
+//part 2
+selectUserNumber.classList.remove("hide");
+selectUserNumber.addEventListener("change", function (e) {
+    const numUsers = e.target.value;
+    getData(numUsers);
+});
